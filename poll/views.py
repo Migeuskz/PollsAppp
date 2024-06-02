85bafb299086ea0a122ccfcdf1476706d36c030c
Miguel Angel Sanchez Papalotzi <msanchezpapalot@gmail.com>
2024-06-01 21:57:52 -0600

    agregando una grafica de pastel a pollsApp

--- poll/views.py
+++ poll/views.py
@@ -1,5 +1,6 @@
+import json
 from django.shortcuts import get_object_or_404, redirect, render
-from .models import Question
+from .models import Question, Choice
 
 # Create your views here.
 def home(request):
@@ -15,25 +16,49 @@ def vote(request, q_id):
     q = get_object_or_404(Question, pk=q_id)
     # q = Question.objects.get(pk=q_id)
     if request.method == 'POST':
-        choice_id = request.POST.get('choice')
-        choice = q.choice_set.get(pk=choice_id)
-        choice.votes += 1
-        choice.save()
-        return redirect('poll:result', q_id)
-        # return render(request, 'poll/result.html',{
-        #     "question": q
-        # })
-        
+        try:
+            choice_id = request.POST.get('choice')
+            choice = q.choice_set.get(pk=choice_id)
+            choice.votes += 1
+            choice.save()
+            return redirect('poll:result', q_id)
+        except(KeyError, Choice.DoesNotExist):
+            return render(request, 'poll/vote.html', {
+                "question": q,
+                "error_message": "Debes elegir algo :0"
+            })  
     return render(request, 'poll/vote.html',
                   {
                       "question": q
                   })
 
 def result(request, q_id):
+    q = get_object_or_404(Question, pk=q_id)
+    
     try:
         q = Question.objects.get(pk=q_id)
     except Question.DoesNotExist:
         return redirect('poll:home')
+    
+    labels = []
+    data = []
+    
+    
+    for item in q.choice_set.all():
+        labels.append(item.choice_text)
+        data.append(item.votes)
+    
+    info ={
+        'labels': labels,
+        'data': data,
+    }
+        
+    
     return render(request, 'poll/result.html', {
-        "question": q
+        "question": q,
+        
+        "info": json.dumps(info)
     })
+    
+    
+    
